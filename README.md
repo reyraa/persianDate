af9aff130ce29533313b2c59acc81dc995f1d59c
ali haghighatkhah <alihaghighatkhah@yahoo.com>
2014-06-10 10:40:15 +0430

    defining methods similar to Javascript Date object

--- README.md
+++ README.md
@@ -5,32 +5,31 @@
 Just link the library to your page it's done!
 
 ## Methods
-**1)** getDate() 
-	> returns an object containing the full date parameters and a string prpperty in the format of yyyy/mm/dd
-
-
-**Example:**
-```javascript
-persianDate.getDate();
-persianDate.getDate().stringified;
-````
+**1)** getDay() 
+	> returns a number between 0 and 6, representing the day in week. pass 'name' as parameter and it'll return the name of the day in Persian
 -----
-**2)**getYear()
-	>returns a four digit number as year
-
-**Example:**
-```javascript
-persianDate.getYear();
-````
+**2)** getDate() 
+	> returns a number between 1 and 31, representing the day in month.
 -----
 **3)**getMonth()
-	>returns a number between 1 and 12 as month
-**4)**getDay()
-	>returns a number between 1 and 31 as the day in month
+	>returns a number between 0 and 11 representing the month in year. pass 'name' as parameter and it'll return the name of the month in Persian
+-----
+**4)**getYear()
+	>returns a four digit number as year. also you can pass the string 'short', and get the year in two digits
+-----
+**5)**getFullDate()
+	>returns an object containing day , month and year.
+-----
+**6)**toJSON()
+	>returns astring in the standard Persian date format: yyyy/mm/dd
+-----
+
 
 
  * **v0.1.0** - 2014-06-05
    - First commit.
+ * **v0.1.1** - 2014-06-10
+   - defining methods similar to Javascript Date object
 
 ## Author
 **Ali Haghighatkhah**
--- persianDate.js
+++ persianDate.js
@@ -3,22 +3,26 @@
 		var esfand = ( (gYear-2013)%4 == 0 ) ? 30 : 29;
 		var gArray = [31,28,31,30,31,30,31,31,30,31,30,31];
 		var pArray = [31,31,31,31,31,31,30,30,30,30,30,esfand];
+		var weekNames = ['یکشنبه' , 'دوشنبه' , 'سه شنبه' , 'چهارشنبه' , 'پنج شنبه' , 'آدینه' , 'شنبه']
+		var monthNames = ['روردین' , 'اردیبهشت' , 'خرداد' , 'تیر' , 'مرداد' , 'شهریور' , 'مهر' , 'آبان' , 'آذر' , 'دی' , 'بهمن' , 'اسفند']
 		var passedDayes = 0;
 		var gDate = null;
 		gDate = new Date();
 		var gDay = gDate.getDate();
 		var gMonth = gDate.getMonth()+1;
 		var gYear = gDate.getFullYear();
-		window.persianDate = 
+		function createDate()
 		{
-			year: 0,
-			month: 0,
-			day: 0,
-			
-			
-			
+			window.persianDate = 
+			{
+				year: 0,
+				month: 0,
+				day: 0,
+				
+			}
+				
 			//check if we're in new persian year or not
-			nextYear : function()
+			function nextYear()
 			{
 				if( gMonth<=2)
 				{
@@ -33,76 +37,90 @@
 					return true;
 				}
 			}
-		}
-		if(persianDate.nextYear() )
-		{
+			
+			if(nextYear() )
+			{
 
-			//define the year
-			persianDate.year = gYear - 621;
-			//define the persian month and day
-			for(var i=2 ; i <= gMonth-2 ; i++)
-			{
-				passedDayes+= gArray[i];
-			}
-			passedDayes = passedDayes + gDay - leap+1;
-			i = 0;
-			while(passedDayes > 0)
-			{
-				passedDayes -= pArray[i];
-				persianDate.month +=1;
-				i++;
-			}
-			persianDate.month;
-			persianDate.day = pArray[persianDate.month] + passedDayes;
-		}
-		else
-		{
-			//define the year
-			persianDate.month = gYear - 622;
-			//define the persian month and day
-			for(var i=0 ; i <= gMonth-2 ; i++)
-			{
-				passedDayes+= gArray[i];
-			}
-			passedDayes += gDay;
-			//after defining the number of days passed from georgian year
-			// we need to dubtract it from the number of days between Persian and Georgian new years
-			// 31(jan) + 28(feb) + leap -1(the leap day itself)
-			passedDayes = 58 + leap - passedDayes;
-			i = 11;//last month in array
-			while(passedDayes > 0)
-			{
-				passedDayes -= pArray[i];
-				persianDate.month = i+1;
-				i--;
-			}
-			if(passedDayes == 0)
-			{
-				persianDate.month --;
-				persianDate.day = pArray[persianDate.month-1];
+				//define the year
+				persianDate.year = gYear - 621;
+				//define the persian month and day
+				for(var i=2 ; i <= gMonth-2 ; i++)
+				{
+					passedDayes+= gArray[i];
+				}
+				passedDayes = passedDayes + gDay - leap+1;
+				i = 0;
+				while(passedDayes > 0)
+				{
+					passedDayes -= pArray[i];
+					persianDate.month +=1;
+					i++;
+				}
+				persianDate.month;
+				persianDate.day = pArray[persianDate.month] + passedDayes;
 			}
 			else
 			{
-				persianDate.day = passedDayes*-1;
+				//define the year
+				persianDate.month = gYear - 622;
+				//define the persian month and day
+				for(var i=0 ; i <= gMonth-2 ; i++)
+				{
+					passedDayes+= gArray[i];
+				}
+				passedDayes += gDay;
+				//after defining the number of days passed from georgian year
+				// we need to dubtract it from the number of days between Persian and Georgian new years
+				// 31(jan) + 28(feb) + leap -1(the leap day itself)
+				passedDayes = 58 + leap - passedDayes;
+				i = 11;//last month in array
+				while(passedDayes > 0)
+				{
+					passedDayes -= pArray[i];
+					persianDate.month = i+1;
+					i--;
+				}
+				if(passedDayes == 0)
+				{
+					persianDate.month --;
+					persianDate.day = pArray[persianDate.month-1];
+				}
+				else
+				{
+					persianDate.day = passedDayes*-1;
+				}
 			}
-		}
-			
-		persianDate.getDay = function()
+		}//createDate
+		createDate();
+		///Methods
+		//I've tried to make these methods similar to those in javascript Date object
+		persianDate.getDate = function()
 		{
 			return persianDate.day;
 		}
-		persianDate.getMonth = function()
+		persianDate.getDay = function(type)
 		{
-			return persianDate.month;
+			var dayInWeek = gDate.getDay();
+			
+			return (type === 'name') ? weekNames[dayInWeek] : dayInWeek;
 		}
-		persianDate.getYear = function()
+		persianDate.getMonth = function(type)
 		{
-			return persianDate.year;
+			return (type === 'name') ? monthNames[persianDate.month-1] : persianDate.month;
 		}
-		persianDate.getDate = function()
+		persianDate.getYear = function(type)
 		{
-			var temp = {'year' : persianDate.year , 'month' : persianDate.month , 'day' : persianDate.day , 'stringified' : persianDate.year +'/'+ persianDate.month +'/'+ persianDate.day};
+		
+			return (type === 'short') ? (persianDate.year % 100) : persianDate.year;
+		}
+		persianDate.getFullDate = function()
+		{
+			var temp = {'year' : persianDate.year , 'month' : persianDate.month , 'day' : persianDate.day};
 			return temp;
+		}
+		persianDate.toJSON = function()
+		{
+			return (persianDate.year +'/'+ persianDate.month +'/'+ persianDate.day);
 		}	
 			
 			//now we have number of days passed from georgian year.
